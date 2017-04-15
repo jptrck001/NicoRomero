679ba889964ca80203435ca98f6cd0c1ef0a32e7
raulstriglio <raul.striglio@gmail.com>
2017-04-15 18:29:17 -0300

    LAst fixes

--- app/build/generated/source/apt/debug/com/adedo/Calendar_activity$$ViewBinder.java
+++ app/build/generated/source/apt/debug/com/adedo/Calendar_activity$$ViewBinder.java
@@ -8,8 +8,8 @@ import butterknife.ButterKnife.ViewBinder;
 public class Calendar_activity$$ViewBinder<T extends com.adedo.Calendar_activity> implements ViewBinder<T> {
   @Override public void bind(final Finder finder, final T target, Object source) {
     View view;
-    view = finder.findRequiredView(source, 2131493064, "field 'widget'");
-    target.widget = finder.castView(view, 2131493064, "field 'widget'");
+    view = finder.findRequiredView(source, 2131493063, "field 'widget'");
+    target.widget = finder.castView(view, 2131493063, "field 'widget'");
     view = finder.findRequiredView(source, 2131493040, "field 'publicados'");
     target.publicados = finder.castView(view, 2131493040, "field 'publicados'");
   }
